4897f567f5f9878352a63a0fdc75fb76c3b1478b
punthanat <punthanat@hotmail.com>
2021-04-16 17:16:26 +0700

    [62130500058] Add component and local, global registration

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-
+import { createApp } from 'vue'
+import App from './App.vue'
 import './assets/tailwind.css'
+import frame from './components/frame.vue'
 
-
-const app = createApp(App).use(router)
+const app = createApp(App)
+app.component('frame', frame)
 app.mount('#app')
