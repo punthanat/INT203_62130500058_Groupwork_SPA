d730017733220c602d6283fddc2d04b08d4709a9
punthanat <punthanat@hotmail.com>
2021-04-16 17:13:46 +0700

    [62130500058] Add all basic

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+
 import './assets/tailwind.css'
-import frame from './components/frame.vue'
+
 
 const app = createApp(App).use(router)
-app.component('frame', frame)
-app.use(router).mount('#app')
+app.mount('#app')
